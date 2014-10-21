9858731602ff7c6f8f4adda9cd2e33607a62534c
yangjl0930@gmail.com <yangjl0930@gmail.com>
2014-10-20 23:45:11 -0700

    phenotype

--- README.md
+++ README.md
@@ -8,7 +8,16 @@
 * **DTP**: days to pollen
 * **ASI**: anthesis-silk Interval
 * **Adj.GY**: adjusted grain yield
-* **TW**: _test weight?_
+* **TW**: _test weight?_ what is this trait? 
+
+
+### Jinliang's report on phenotypic data checking
+
+```{r, eval=FALSE}
+source("~/Documents/Github/pvpDiallel/munge/1.raw_pheno/1.A.1_format_pheno.R")
+```
+
+
 
 
 
--- munge/1.raw_pheno/1.A.1_format_pheno.R
+++ munge/1.raw_pheno/1.A.1_format_pheno.R
@@ -2,7 +2,6 @@
 ## Oct. 13th, 2014
 ## phenotypic data of offpvp diallel
 
-setwd("/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/")
 
 ### pheno per se
 
@@ -12,19 +11,20 @@ pmatrix <- function(infile="Gvalues_Adj.GY", trait="GY"){
   pheno <- read.table(infile, header=TRUE)
   names(pheno) <- nm
   pheno$trait <- trait
-  message(sprintf("# [ %s ] rows loaded!", nrow(pheno)))
+  message(sprintf("# [ %s ] rows loaded for trati [ %s ]!", nrow(pheno), trait))
   return(pheno)  
 } 
 
 ###
-gy <- pmatrix(infile="Gvalues_Adj.GY", trait="GY")
-asi <- pmatrix(infile="Gvalues_ASI", trait="ASI")
-dtp <- pmatrix(infile="Gvalues_DTP", trait="DTP")
-dts <- pmatrix(infile="Gvalues_DTS", trait="DTS")
-eht <- pmatrix(infile="Gvalues_EHT", trait="EHT")
-pht <- pmatrix(infile="Gvalues_PHT", trait="PHT")
-tw <- pmatrix(infile="Gvalues_TW", trait="TW")
+gy <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_Adj.GY", trait="GY")
+asi <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_ASI", trait="ASI")
+dtp <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_DTP", trait="DTP")
+dts <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_DTS", trait="DTS")
+eht <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_EHT", trait="EHT")
+pht <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_PHT", trait="PHT")
+tw <- pmatrix(infile="/Users/yangjl/Box\ Sync/Projects/PVP-Diallel/GeneticValues/Gvalues_TW", trait="TW")
 
 trait <- rbind(gy, asi, dtp, dts, eht, pht, tw)
-write.table(trait, "~/Documents/Github/pvpDiallel/data/trait_matrix.csv", sep=",",
+#head(trait)
+write.table(trait, "data/trait_matrix.csv", sep=",",
             row.names=FALSE, quote=FALSE)
