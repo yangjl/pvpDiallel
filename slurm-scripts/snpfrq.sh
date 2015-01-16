c046aafc03db9089f1e93d22e45b7e8e1155b5f8
Jinliang Yang <jolyang@bigmem7.farm.cse.ucdavis.edu>
2015-01-16 14:01:48 -0800

    from farm

--- slurm-scripts/snpfrq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -D /home/jolyang/Documents/pvpDiallel/
-#SBATCH -o /home/jolyang/Documents/pvpDiallel/slurm-log/
-#SBATCH -e /home/jolyang/Documents/pvpDiallel/slurm-log/
-#SBATCH -J snpfrq
-set -e
-set -u
-
-snpfrq -i allsnps_13.7m.dsf2 -s 5 -e 16 -m NH -a 0 -b 1 -o allsnps_frq.out
