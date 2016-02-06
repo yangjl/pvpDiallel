4d63683181b923041a0c9997af6f16b329394347
Jinliang Yang <yangjl0930@gmail.com>
2016-02-05 16:06:45 -0800

    updates

--- profiling/4.GERP-GS/4.1.D_gerp_cMMb.R
+++ profiling/4.GERP-GS/4.1.D_gerp_cMMb.R
@@ -35,3 +35,28 @@ axis(side =1, at =1:2, labels =c("< 0.5", " >= 0.5"))
 dev.off()
 
 
+BINSIZE = 1000000
+gerp$bin <- paste(gerp$chr, round(gerp$pos/BINSIZE, 0), sep="_")
+res <- ddply(gerp, .(bin), summarise,
+             mgerp = mean(RS),
+             gen = 1000000*(max(genetic) - min(genetic))/(max(pos) - min(pos)) )
+range(res$gen)
+
+res <- subset(res, !is.na(gen))
+t.test(subset(res, gen < 0.5)$mgerp, subset(res, gen > 0.5)$mgerp)
+
+cutoff <- quantile(res$gen, c(0.25, 0.5, 0.75))
+res$sq <- 0
+res[res$gen < cutoff[1], ]$sq <- 1
+res[res$gen >= cutoff[1] & res$gen < cutoff[2], ]$sq <- 2
+res[res$gen >= cutoff[2] & res$gen < cutoff[3], ]$sq <- 3
+res[res$gen >= cutoff[3], ]$sq <- 4
+
+pdf("graphs/Fig1d.pdf", width=5, height=5)
+#par(mar = c(7, 4, 4, 2) + 0.1)
+beanplot(mgerp ~ sq, data = res, kernel="cosine", ll = 0.04, cex=1.5, side = "no", cut=10, ylim=c(0.5, 1.5),
+         border = NA, col="antiquewhite3", xaxt="n", ylab="GERP Score", xlab="Recombination Rate (cM/Mb)")
+#axis(side =1, at =1:4, las=3, labels =c("[0, 25%)", "[25%, 50%)", "[50%, 75%)", "[75%, 100%)"))
+axis(side =1, at =1:4, labels =c("<25%", "25-50%", "50-75%", " >75%"), cex.lab=0.25)
+
+dev.off()
