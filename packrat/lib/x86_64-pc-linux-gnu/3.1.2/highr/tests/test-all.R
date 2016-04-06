c3cc901e8a246d9b324d0169181cec85d5bd277f
Jinliang Yang <yangjl0930@gmail.com>
2016-04-06 12:13:38 -0700

    del some unused folders

--- packrat/lib/x86_64-pc-linux-gnu/3.1.2/highr/tests/test-all.R
+++
@@ -1,3 +0,0 @@
-library(testit)
-
-test_pkg('highr', if (getRversion() >= '3.0.0') 'testit' else 'test-R2')
--- packrat/lib/x86_64-pc-linux-gnu/3.1.3/Rsamtools/tests/Rsamtools_unit_tests.R
+++
@@ -1,2 +0,0 @@
-require("Rsamtools") || stop("unable to load Rsamtools package")
-Rsamtools:::.test()
