67af87708992249fa56e6152cb4e00e566895e3c
Jinliang Yang <yangjl0930@gmail.com>
2016-06-14 14:55:45 -0700

    finish BPH plot

--- profiling/4.GERP-GS/4.8.B_bph_k_gerp_fig2ab.R
+++ profiling/4.GERP-GS/4.8.B_bph_k_gerp_fig2ab.R
@@ -21,7 +21,7 @@ nx_flt <- function(res=dat, x=5){
   for(i in 1:7){
     sub <- subset(res, trait==traits[i])
     tot <- sum(sub$H2_mrk)
-    tem <- subset(sub, H2_mrk > 5*tot/nrow(sub))
+    tem <- subset(sub, H2_mrk > x*tot/nrow(sub))
     out <- rbind(out, tem)
   }
   out$trait <- toupper(out$trait)
@@ -29,9 +29,9 @@ nx_flt <- function(res=dat, x=5){
 }
 ##########################################
 
-dat <- read.csv("largedata/lcache/kval_perse_0x.csv")
+dat <- read.csv("largedata/lcache/kval_bph_0x.csv")
 res1 <- getvar(res=dat)
-res2 <- nx_flt(res=dat, x=5)
+res2 <- nx_flt(res=dat, x=1)
 res2 <- subset(res2, abs(k) < 2)
 dim(res2) #107346      8
 #############################################
@@ -66,9 +66,18 @@ p2 <- ggplot(data=res2) +
   coord_flip() + xlab("Degree of Dominance (k)") + ylab("") + facet_grid(~ trait) 
 
 ########
-pdf("graphs/Fig2_eff_var.pdf", width=13, height=5)
+pdf("graphs/SFig2_eff_var.pdf", width=13, height=5)
 multiplot(p1, p2, cols=2)
 dev.off()
+
+
+
+
+
+
+
+
+
 
 ########################
 
@@ -107,25 +116,6 @@ excess_pos <- function(dat, myt="TW", cutoff=0){
 excess_pos(dat=res2, myt="GY", cutoff=0) #0.4598517
 excess_pos(dat=res2, myt="GY", cutoff=1) #0.1057723
 
-#########################################
-####### for BAPG meeting
-theme_set(theme_grey(base_size = 18)) 
-
-res2 <- subset(res2, trait %in% c("DTP", "PHT", "GY"))
-res2 <- subset(res2, abs(k) < 2)
-res2$trait <- toupper(res2$trait)
-res2$trait <- factor(res2$trait, levels=bymed$trait)
-p2 <- ggplot(data=res2) +
-  geom_density(aes(x= k, y=-..scaled.., fill= as.factor(trait)) ) +
-  #guides(fill=FALSE) +
-  labs(y=NULL, fill="Trait") + theme_bw() +
-  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
-  coord_flip() + xlab("Degree of Dominance (k)") + ylab("") + facet_grid(~ trait) 
-
-########
-pdf("graphs/Fig_BAPG.pdf", width=6, height=5)
-p2
-dev.off()
 
 
 
